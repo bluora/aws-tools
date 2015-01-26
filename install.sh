228ad74b60685006a5f640278fc74c88bc0c11d2
Rocco Howard <rocco@bluora.com.au>
2015-01-26 20:50:08 +1000

    Only install pcakages if not present

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ else
 	sudo yum install git unzip wget curl -y
 fi
 
-cd /usr/local/src/ && sudo curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && sudo unzip awscli-bundle.zip && sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
-cd /usr/local/src/ && sudo wget -q http://s3.amazonaws.com/ec2metadata/ec2-metadata && sudo chmod +x ec2-metadata && sudo mv ec2-metadata /usr/bin
+command -v aws >/dev/null 2>&1 || { cd /usr/local/src/ && sudo curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && sudo unzip awscli-bundle.zip && sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws exit 0; }
+command -v ec2-metadata >/dev/null 2>&1 || { cd /usr/local/src/ && sudo wget -q http://s3.amazonaws.com/ec2metadata/ec2-metadata && sudo chmod +x ec2-metadata && sudo mv ec2-metadata /usr/bin exit 0; }
 
 sudo ln -s /usr/local/aws-tools/bin/aws-get-instances /usr/bin/aws-get-instances
 sudo ln -s /usr/local/aws-tools/bin/aws-run-remote-command /usr/bin/aws-run-remote-command
